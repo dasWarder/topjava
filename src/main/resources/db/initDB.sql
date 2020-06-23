78e39513cad9454b8fc07cebec97385f119f188e
Andrey Babichev <Al.94_94@mail.ru>
2020-06-23 22:45:12 +0300

    HW03 NON OPTIONAL COMMON PART

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,6 +1,9 @@
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
+DROP SEQUENCE IF EXISTS global_seq_meal;
+
 
 CREATE SEQUENCE global_seq START WITH 100000;
 
@@ -22,4 +25,17 @@ CREATE TABLE user_roles
   role    VARCHAR,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE SEQUENCE global_seq_meal START WITH 1;
+
+CREATE TABLE meals
+(
+    id              INTEGER PRIMARY KEY DEFAULT nextval('global_seq_meal'),
+    id_user         INTEGER     NOT NULL,
+    date_time       TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
+    description     VARCHAR     NOT NULL,
+    calories        INTEGER     NOT NULL,
+
+    FOREIGN KEY (id_user) REFERENCES users (id) ON DELETE CASCADE
 );
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,6 +1,9 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
+ALTER SEQUENCE global_seq_meal RESTART WITH 1;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
+
 
 INSERT INTO users (name, email, password) VALUES
   ('User', 'user@yandex.ru', 'password'),
@@ -9,3 +12,9 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (id_user, date_time, description, calories) VALUES
+    (100000, '25.11.2020 10:33:00', 'Lunch', 600),
+    (100000, '21.12.2020 10:38:00', 'Lunch', 500),
+    (100000, '22.12.2020 11:00:00', 'Breakfast', 600),
+    (100000, '26.11.2020 12:10:00', 'Fastfood', 900);
