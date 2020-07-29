23d02f68204886094268c25741978a145b46de59
Andrey Babichev <Al.94_94@mail.ru>
2020-07-29 15:40:03 +0300

    Create resource controller test and create style.css test

---
+++ src/test/java/ru/javawebinar/topjava/web/ResourceControllerTest.java
@@ -0,0 +1,23 @@
+package ru.javawebinar.topjava.web;
+
+import jdk.jfr.ContentType;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+public class ResourceControllerTest extends AbstractControllerTest{
+
+    @Test
+    void getStyle() throws Exception {
+        perform(get("/resources/css/style.css"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/css"));
+    }
+}
